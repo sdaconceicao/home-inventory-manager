49a06248e998acd8d62ea5a1f0e260fbf4dc3e8e
Stephen da Conceicao <daconcei472@gmail.com>
2016-04-10 10:25:13 -0400

    Oath configured for facebook login

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
 /* global module, require */
 'use strict';
 
-let templates = require('../dist/templates'),
+let oath = require('./config/constants.js'),
+    templates = require('../dist/templates'),
     components = require('./components/components'),
     home = require('./modules/home/home'),
     login = require('./modules/login/login'),
@@ -11,12 +12,13 @@ let templates = require('../dist/templates'),
 ;
 
 angular.module('him', dependencies)
-    .config(/* @ngInject */($locationProvider, $urlRouterProvider, $compileProvider, $authProvider)=>{
+    .constant('oath', oath)
+    .config(/* @ngInject */($locationProvider, $urlRouterProvider, $compileProvider, $authProvider, oath)=>{
         $locationProvider.html5Mode({enabled: true, requireBase: false});
         $urlRouterProvider.otherwise('/home');
         $compileProvider.debugInfoEnabled(false);
         $authProvider.facebook({
-            clientId: 'Facebook App ID',
+            clientId: oath.facebookId,
             responseType: 'token'
         });
     })
